810278cd91cfecf493f2efb23e909f8fd18bbe33
Ayush Gera <ayush.gera@amadeus.com>
2015-08-25 17:01:11 +0530

    all code

---
+++ www/js/main.js
@@ -0,0 +1,291 @@
+//var paneArray=["pane"]
+/**
+ * Set button action to trigger vTiwari like & dislike.
+ */
+$('.actions .like, .actions .dislike').click(function(e){
+	e.preventDefault();
+	$("#sliderContainer").vTiwari($(this).attr('class'));
+});
+
+
+(function(){
+	$("#map-canvas").hide();
+	$("#likesPage").hide();
+	$("#description-page").hide();
+	
+	loadInitialImages();
+	var loadedImageId=$("#events_list li").last().attr("id");
+			$("#back-cover").css("background","url('../www/images/main/"+loadedImageId.substring(0,loadedImageId.length-1)+"/"+loadedImageId.substring(loadedImageId.length,loadedImageId.length-1)+".jpg')  no-repeat 0px 0px");
+			$("#back-cover").css("background-size","cover");
+			  $("#back-cover").css("filter","blur(5px)");
+			  $("#back-cover").css("-webkit-filter","blur(5px)");
+})();
+
+function shuffleArray(img,index,categ,prev){
+	for(var i=index+1;i<img.length;i++){
+		if(img[i].category==categ){
+			var imgTemp=img[prev];
+			img[prev]=img[i];
+			img[i]=imgTemp;
+			if($(img[prev].id).hasClass("lastItem")){
+				$(img[prev].id).removeClass("lastItem");
+				updateLastItem();
+			}
+			break;
+		}
+	}
+}
+
+function updateLastItem(){
+	$("#events_list :first-child").addClass("lastItem");
+	
+}
+
+function putInDatabase(map){
+	window.localStorage.setItem("categoryCount",JSON.stringify(map));
+}
+
+
+
+function categoryCount(categoryId,like){
+	var category= categoryId.substring(0,categoryId.length-1);
+	if(like){
+		if(typeof (categoryCountMap[category])!=="undefined"){
+			categoryCountMap[category]+=1;
+		}else{
+			categoryCountMap[category]=1;
+		}
+		
+	}else{
+		if(typeof (categoryCountMap[category])!=="undefined"){
+			categoryCountMap[category]-=1;
+		}else{
+			categoryCountMap[category]=-1;
+		}
+	}
+}
+
+function orderArray(index,arr,category)
+{
+	for(var x=index;x>=0;x--)
+	{
+		if(imagesArr[x].category==category){
+			var prev = imagesArr[x];
+			imagesArr[x]=imagesArr[index];
+			imagesArr[index]=prev;
+			if($("#"+imagesArr[index].id).hasClass("lastItem")){
+				$("#"+imagesArr[index].id).removeClass("lastItem");	
+				updateLastItem();
+			}
+				imagesArr.splice(index+1,1);
+				loadInitialImages();
+			break;
+		}
+	}
+
+}
+
+
+$("#sliderContainer").vTiwari({
+	// dislike callback
+    onDislike: function (item) {
+	    // Perform some logic
+		categoryCount(item.attr("id"),false);
+	if(currentImage!=0){
+			var loadedImageId=imagesArr[--currentImage].id;
+			$("#back-cover").css("background","url('../www/images/main/"+loadedImageId.substring(0,loadedImageId.length-1)+"/"+loadedImageId.substring(loadedImageId.length,loadedImageId.length-1)+".jpg')  no-repeat 0px 0px");
+						$("#back-cover").css("background-size","cover");
+	}
+	   if(item.hasClass("lastItem")){
+			putInDatabase(categoryCountMap);
+			pageNavigation();
+	   }
+    },
+	// like callback
+    onLike: function (item) {
+	    // Perform some logic
+        likedIds.push(item.attr("id"));	
+		likedTitles.push($("#"+item.attr("id")+" font").html());	
+	categoryCount(item.attr("id"),true);
+	if(currentImage!=0){
+			var loadedImageId=imagesArr[--currentImage].id;
+			$("#back-cover").css("background","url('../www/images/main/"+loadedImageId.substring(0,loadedImageId.length-1)+"/"+loadedImageId.substring(loadedImageId.length,loadedImageId.length-1)+".jpg')  no-repeat 0px 0px");
+						$("#back-cover").css("background-size","cover");
+	}
+	
+		/*
+		for(var i=imagesArr.length-1,j=i-1;i>=0 && j>=0;i--,j--){
+			if(imagesArr[i].id==item.attr("id")){
+				imagesArr[i].like="1";
+				var category =  item.attr("id").substring(0,item.attr("id").length-1);
+				if(imagesArr[j].category!=category){
+					orderArray(j,imagesArr,category);
+				}
+			}
+		}*/		
+		if(item.hasClass("lastItem")){
+			putInDatabase(categoryCountMap);
+			pageNavigation();
+		}		
+    },
+	animationRevertSpeed: 200,
+	animationSpeed: 400,
+	threshold: 1,
+	likeSelector: '.like',
+	dislikeSelector: '.dislike'
+});
+
+function pageNavigation(){
+	$("#page1").hide();
+	$(".wrap").hide();
+	$(".actions").hide();
+	//onSuccess(); //loads the map
+	//$("#map-canvas").show();
+	updateLikedItems();
+	$("#likesPage").show();
+}
+
+function reStructureImagesArr(imagesArr,categoryCountMap){
+	sortedMap= sortCategoryCountMap(categoryCountMap);
+	for(var i=0;i<sortedMap.length;i++){
+		sortedMapObject[sortedMap[i][0]]=sortedMap[i][1];
+	}
+	imagesArr.sort(compare);
+}
+
+function compare(a,b) {
+  if (sortedMapObject[a.category] < sortedMapObject[b.category])
+    return -1;
+  if (sortedMapObject[a.category] > sortedMapObject[b.category])
+    return 1;
+  return 0;
+}
+
+
+function sortCategoryCountMap(map){
+	var sortable = [];
+	for (var category in map)
+		  sortable.push([category, map[category]])
+	sortable.sort(function(a, b) {return a[1] - b[1]})
+	return sortable;
+}
+
+
+function loadInitialImages(){
+    if(localStorage.getItem("categoryCount") !== null){
+        reStructureImagesArr(imagesArr,JSON.parse(localStorage.getItem("categoryCount")));
+    }
+    
+    /*if(imagesArr.length===0){
+     imagesArr = [{"id":"adventure0","category":"adventure","event":"","like":"0"},
+     {"id":"food0","category":"food","event":"","like":"0"},
+     //{"id":"religion0","category":"religion","event":"","like":"0"},
+     {"id":"music0","category":"music","event":"","like":"0"},
+     //{"id":"sports0","category":"sports","event":"","like":"0"},
+     {"id":"adventure1","category":"adventure","event":"","like":"0"},
+     {"id":"food1","category":"food","event":"","like":"0"},
+     //{"id":"religion1","category":"religion","event":"","like":"0"},
+     {"id":"music1","category":"music","event":"","like":"0"},
+     //{"id":"sports1","category":"sports","event":"","like":"0"},
+     {"id":"adventure2","category":"adventure","event":"","like":"0"},
+     {"id":"food2","category":"food","event":"","like":"0"},
+     //{"id":"religion2","category":"religion","event":"","like":"0"},
+     {"id":"music2","category":"music","event":"","like":"0"},
+     //{"id":"sports2","category":"sports","event":"","like":"0"},
+     {"id":"adventure3","category":"adventure","event":"","like":"0"},
+     {"id":"food3","category":"food","event":"","like":"0"},
+     //{"id":"religion3","category":"religion","event":"","like":"0"},
+     {"id":"music3","category":"music","event":"","like":"0"},
+     //{"id":"sports3","category":"sports","event":"","like":"0"},
+     {"id":"adventure4","category":"adventure","event":"","like":"0"},
+     {"id":"food4","category":"food","event":"","like":"0"},
+     //{"id":"religion4","category":"religion","event":"","like":"0"},
+     {"id":"music4","category":"music","event":"","like":"0"},
+     //{"id":"sports4","category":"sports","event":"","like":"0"}
+     ];
+     }
+     else{
+     $("ul li").remove();
+     }*/
+    var imagePath = "../www/images/main";
+    for(var i=0;i<imagesArr.length;i++){
+        var panelClass="pane"+(i+1);
+        var image = imagesArr[i];
+        var imageId =  image.id.replace ( /[^\d]/g, '' );
+        var img = parseInt(imageId);
+        var imageSrc = imagePath+"/"+image.category+"/"+img+".jpg";
+        var  li = document.createElement('li');
+        var imageDiv= document.createElement('div');
+        var titleDiv = document.createElement('div');
+        var titleFont = document.createElement('font');
+		
+        $("<font style='font-size:1em; color:#fff; font-weight:500; margin-bottom:0.5em; font-family: 'Raleway', sans-serif; padding: 4px 13px 7px; word-wrap: break-word;'>"+imagesArr[i].title+"</font>").appendTo(titleDiv);
+        $(titleDiv).css("background-color","rgba(0,0,0,0.8)");
+		$(titleDiv).css("width","100%");
+		$(titleDiv).css("padding","1em");
+		$(titleDiv).css("position","absolute");
+		$(titleDiv).css("bottom","0");
+		$(titleDiv).children().css("color","white");
+        $(li).attr("id",image.id);
+        $(imageDiv).css("background","url(\'"+imageSrc+"\') no-repeat scroll center center");
+        $(imageDiv).addClass("img");
+        $(imageDiv).css("background-size", "cover");
+        $(imageDiv).append(titleDiv);
+        var likeDiv= document.createElement('div');
+        $(likeDiv).addClass("like");
+        var dislikeDiv= document.createElement('div');
+        $(dislikeDiv).addClass("dislike");
+        li.appendChild(imageDiv);
+        li.appendChild(likeDiv);
+        $(li).addClass(panelClass);
+        li.appendChild(dislikeDiv);
+        var ul = document.getElementById("events_list");
+        var divCont = document.getElementById("sliderContainer");
+        var wrap = document.getElementById("mainBody");
+        ul.appendChild(li);
+        divCont.appendChild(ul);
+        wrap.appendChild(divCont);
+        
+        if(i===0){
+            $(li).addClass("lastItem");
+        }
+    }
+    
+}
+
+
+
+function updateLikedItems(){
+	var music=0,history=0,adventure=0,sports=0,food=0,religion=0;
+	for(var i=0;i<likedIds.length;i++){
+		var category=likedIds[i].substring(0, likedIds[i].length-1);
+		switch(category){
+			case "music": music++; break;
+			case "history": history++; break;
+			case "adventure": adventure++; break;
+			case "sports": sports++; break;
+			case "food": food++; break;
+			case "religion": religion++; break;
+		}
+	}
+	
+	$("li[data-item='music'] .numLikes").html(music);
+		$("li[data-item='history'] .numLikes").html(history);
+			$("li[data-item='adventure'] .numLikes").html(adventure);
+				$("li[data-item='sports'] .numLikes").html(sports);
+					$("li[data-item='food'] .numLikes").html(food);
+						$("li[data-item='religion'] .numLikes").html(religion);
+}
+
+function openMap(category){
+	
+	for(var i=0;i<likedIds.length;i++){
+		if(likedIds[i].substring(0, likedIds[i].length-1) === category){
+			categoryIds.push(likedIds[i]);
+			categoryTitles.push(likedTitles[i]);
+			onSuccess(); //loads the map
+			$("#map-canvas").show();
+			$("#likesPage").hide();
+		}
+	}
+}
---
+++ www/js/places.js
@@ -0,0 +1,275 @@
+/*$(document).ready(function(){
+  //$('.bxslider').bxSlider();
+  
+  	slider = $('.bxslider').bxSlider({
+	pager:false,
+	    minSlides: 1,
+  maxSlides: 1,
+  slideWidth: 250,
+  slideMargin: 3
+	});
+	
+	$('#slider-next').click(function(){
+	  slider.goToNextSlide();
+	  return false;
+	});
+
+	$('#slider-count').click(function(){
+	  var count = slider.getSlideCount();
+	  alert('Slide count: ' + count);
+	  return false;
+	});
+  
+}); */
+
+
+///////////////////////////////////////////////////////////////
+
+function CustomMarker(latlng, map, args) {
+	this.latlng = latlng;	
+	this.args = args;	
+	this.setMap(map);	
+}
+
+CustomMarker.prototype = new google.maps.OverlayView();
+
+CustomMarker.prototype.draw = function() {
+	
+	var self = this;
+	
+	var div = this.div;
+	
+	if (!div) {
+	
+		div = this.div = document.createElement('div');
+		
+		div.className = 'marker';
+		div.id = self.args.image;
+		div.style.position = 'absolute';
+		div.style.cursor = 'pointer';
+		//div.style.width = '40px';
+		//div.style.height = '40px';
+		//div.style.padding = '3px';
+		//div.style.backgroundColor = 'white';
+		//div.style.background = 'blue';
+		//div.style.backgroundImage= "url('las_vegas.jpg')";
+		var elem = document.createElement("img");
+		elem.src= "images/m3.png"
+		//elem.src = "images/"+self.args.image;
+		elem.setAttribute("height", "64px !important");
+		elem.setAttribute("width", "40px !important");
+		div.appendChild(elem);
+		var text = document.createElement("div");
+		$(text).html("<font style='padding:2px;font-weight:900;background-color:white;font-size:1em;border: 2px solid black;'>"+self.args.title+"</font>");
+
+		
+		div.appendChild(text);
+		if (typeof(self.args.marker_id) !== 'undefined') {
+			div.dataset.marker_id = self.args.marker_id;
+		}
+		
+		
+
+		
+		
+		google.maps.event.addDomListener(div, "click", function(event) {
+			//alert('You clicked on a custom marker!' + div.id);	
+			
+			/*find image to load*/
+			
+		$("#map-canvas").hide();
+		$("body").css("overflow","auto");
+		$(".banner").css("background-image","url('../www/images/"+self.args.image+"')");
+		$("#description-page h1 span").html(self.args.title);
+		$("#description-page").show();
+			
+			
+			/*find image END*/
+			/*jQuery('#element_to_pop_up').bPopup({
+                    appendTo: 'form'
+                    , zIndex: 2
+                    , modalClose: false,
+					content:'image',
+					contentContainer:'.content',
+					loadUrl:'images/'+div.id
+                });	*/		
+			google.maps.event.trigger(self, "click");
+		});
+		
+		var panes = this.getPanes();
+		panes.overlayImage.appendChild(div);
+	}
+	
+	var point = this.getProjection().fromLatLngToDivPixel(this.latlng);
+	
+	if (point) {
+		div.style.left = (point.x - 10) + 'px';
+		div.style.top = (point.y - 20) + 'px';
+	}
+};
+
+CustomMarker.prototype.remove = function() {
+	if (this.div) {
+		this.div.parentNode.removeChild(this.div);
+		this.div = null;
+	}	
+};
+
+CustomMarker.prototype.getPosition = function() {
+	return this.latlng;	
+};
+
+///////////////////////////////////////////////////////////
+
+
+
+
+/**
+*	CHICAGO 41.8369, 87.6847
+*	LAS VEGDAS 36.1215,115.1739
+*   MIAMI 25.7753, 80.2089
+*	NYC 40.7127, 74.0059
+*	SAN FRANCISCO 37.7833, 122.4167
+*/
+
+function onSuccess() {
+	/*var element = document.getElementById('geolocation');
+	element.innerHTML = 'Latitude: ' + position.coords.latitude + '<br />' +
+	'Longitude: ' + position.coords.longitude + '<br />' +
+	'<hr />' + element.innerHTML;*/
+	//48.8584831,2.3502476
+	//48.8584831,2.3502476
+	//48.8582, 2.2945
+
+	var lat=48.8582;//position.coords.latitude;
+	var lang=2.2945;//position.coords.longitude;
+	var myLatlng = new google.maps.LatLng(lat,lang);
+	var mapOptions = {zoom: 7,center: myLatlng}
+	var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+	var bounds = new google.maps.LatLngBounds();
+
+	var new_markers=[];
+	//var old_markers=[];
+	var markers_length=0;
+	for(var i=0;i<categoryIds.length;i++){
+		
+		imageUrl= "main/"+categoryIds[i].substring(0, categoryIds[i].length-1)+"/"+categoryIds[i].substring(categoryIds[i].length-1, categoryIds[i].length)+".jpg";
+		new_markers[markers_length++] =new CustomMarker(new google.maps.LatLng(imageMap[categoryIds[i]].latitude, imageMap[categoryIds[i]].longitude), 
+								map,
+								{	image: imageUrl, 
+									title: categoryTitles[i]
+								});
+		//old_markers[markers_length++]=new google.maps.Marker({position: new google.maps.LatLng(imageMap[categoryIds[i]].latitude, imageMap[categoryIds[i]].longitude),map: map});
+		/*google.maps.event.addListener(old_markers[markers_length-1], 'mousedown', function(){
+							//find image END
+				jQuery('#element_to_pop_up').bPopup({
+                    appendTo: 'form'
+                    , zIndex: 2
+                    , modalClose: false,
+					content:'image',
+					contentContainer:'.content',
+					loadUrl:'images/'+imageUrl
+                });
+		});	*/
+		bounds.extend(new google.maps.LatLng(imageMap[categoryIds[i]].latitude, imageMap[categoryIds[i]].longitude));
+	}
+	
+	/*var marker1 = new CustomMarker(new google.maps.LatLng(41.8369, 87.6847), map,{image: 'Chicago.jpg'});
+	//var marker1 = new google.maps.Marker({position: new google.maps.LatLng(41.8369, 87.6847),map: map});
+	bounds.extend(new google.maps.LatLng(41.8369, 87.6847));
+
+	var marker2 = new CustomMarker(new google.maps.LatLng(36.1215,115.1739), map,{image: 'las_vegas.jpg'});
+	//var marker2 = new google.maps.Marker({position: new google.maps.LatLng(36.1215,115.1739),map: map});
+	bounds.extend(new google.maps.LatLng(36.1215,115.1739));
+
+	var marker3 = new CustomMarker(new google.maps.LatLng(25.7753, 80.2089), map,{image: 'Miami.jpg'});
+	//var marker3 = new google.maps.Marker({position: new google.maps.LatLng(25.7753, 80.2089),map: map});
+	bounds.extend(new google.maps.LatLng(25.7753, 80.2089));
+
+	var marker4 = new CustomMarker(new google.maps.LatLng(40.7127, 74.0059), map,{image: 'NewYork.jpg'});
+	//var marker4 = new google.maps.Marker({position: new google.maps.LatLng(40.7127, 74.0059),map: map});
+	bounds.extend(new google.maps.LatLng(40.7127, 74.0059));
+
+	var marker5 = new CustomMarker(new google.maps.LatLng(37.7833, 122.4167), map,{image: 'San_Francisco.jpg'});
+ 	//var marker5 = new google.maps.Marker({position: new google.maps.LatLng(37.7833, 122.4167),map: map});
+	bounds.extend(new google.maps.LatLng(37.7833, 122.4167));*/
+
+ 	map.fitBounds(bounds);
+
+	//map.setCenter(bounds.getCenter(), map.getBoundsZoomLevel(bounds));
+	getZoomByBounds(map,bounds);			
+	/*google.maps.event.addListener(marker1, 'mousedown', function(){
+		slider.goToSlide(0)	;
+	});				
+	google.maps.event.addListener(marker2, 'mousedown', function(){
+		slider.goToSlide(1)	
+	});					
+	google.maps.event.addListener(marker3, 'mousedown', function(){
+		slider.goToSlide(2)	
+	});					
+	google.maps.event.addListener(marker4, 'mousedown', function(){
+		slider.goToSlide(3)	
+	});					
+	google.maps.event.addListener(marker5, 'mousedown', function(){
+		slider.goToSlide(4)	
+	});
+	*/
+		
+	
+}
+
+/**
+* Returns the zoom level at which the given rectangular region fits in the map view. 
+* The zoom level is computed for the currently selected map type. 
+* @param {google.maps.Map} map
+* @param {google.maps.LatLngBounds} bounds 
+* @return {Number} zoom level
+**/
+
+function MProjection() {
+}
+MProjection.prototype.fromLatLngToPoint = function(latlng) {
+    var x = (latlng.lng() + 180) / 360 * 256;
+    var y = ((1 - Math.log(Math.tan(latlng.lat() * Math.PI / 180) + 1 / Math.cos(latlng.lat() * Math.PI / 180)) / Math.PI) / 2 * Math.pow(2, 0)) * 256;
+    return new google.maps.Point(x, y);
+};
+
+function getZoomByBounds( map, bounds ){
+  var MAX_ZOOM =  map.getMapTypeId().maxZoom || 21 ;
+  var MIN_ZOOM =  map.getMapTypeId().minZoom || 0 ;
+
+	
+	// Wait for idle map
+	//google.maps.event.addListener(map, 'idle', function() {
+	   // Get projection
+	  // projection = overlay.getProjection();
+	   
+	     var ne= new MProjection().fromLatLngToPoint( bounds.getNorthEast() );
+	  var sw= new MProjection().fromLatLngToPoint( bounds.getSouthWest() ); 
+
+	  var worldCoordWidth = Math.abs(ne.x-sw.x);
+	  var worldCoordHeight = Math.abs(ne.y-sw.y);
+
+	  //Fit padding in pixels 
+	  var FIT_PAD = 40;
+
+	  for( var zoom = MAX_ZOOM; zoom >= MIN_ZOOM; --zoom ){ 
+		  if( worldCoordWidth*(1<<zoom)+2*FIT_PAD < $(map.getDiv()).width() && 
+			  worldCoordHeight*(1<<zoom)+2*FIT_PAD < $(map.getDiv()).height() )
+			  return zoom;
+	  }
+	   
+	//})
+
+  return 0;
+}
+
+function onError(error) {
+	alert('code: ' + error.code + '\n' +
+	'message: ' + error.message + '\n');
+	}
+//var watchID = navigator.geolocation.watchPosition(onSuccess, onError, {timeout: 10000, enableHighAccuracy: true});
+
+
+
